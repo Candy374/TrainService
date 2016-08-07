2b0866fb5a1375f82396cbd72c5a2e7ba5c2bb13
Shuhang <zhaoshuhang@live.cn>
2016-08-07 23:11:19 +0800

    add dll and test code

---
+++ TrainService/TrainService.Tests/DALTests/GoodsTests.cs
@@ -0,0 +1,35 @@
+﻿using DAL.Entity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainService.Tests.DALTests
+{
+    [TestClass]
+    public class GoodsTests
+    {
+        [TestMethod]
+        public void AddGoodsTest()
+        {
+            GoodsEntity entity = new GoodsEntity
+            {
+                CanChangeFlavor = false,
+                GoodsType = 0,
+                IsAvailable = true,
+                IsObsolete = false,
+                Name = "test yu xiang rou si",
+                OrderCount = 0,
+                PictureUrl = "http://asdasdasdasdasdasdasd",
+                ProviderId = 1,
+                PurchasePrice = 12.00M,
+                Rating = 5,
+                SellPrice = 15.00M
+            };
+
+            DAL.DalFactory.Goods.Add(entity);
+        }
+    }
+}
