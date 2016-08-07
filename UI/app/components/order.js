dd2b509e30672566a53ece64b2ab0de13fae2d8a
Ling Huang <Ling Huang>
2016-08-07 12:53:47 +0800

    setup the build

---
+++ UI/app/components/order.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import Header from '../common/Header.js';
+import Footer from '../common/Footer.js';
+
+export default class OrderPage extends Component {
+    render() {
+        const children  = this.props.children;
+        return (
+            <div>
+                <Header />
+                {children}
+                <Footer />
+            </div>
+        );
+    }
+}
---
+++ UI/app/main.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import OrderPage from './components/order.js';
+
+ReactDOM.render(<OrderPage>test</OrderPage>, 
+    document.getElementById('content'))
