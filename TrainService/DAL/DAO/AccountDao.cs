e5f7236961e4f4e540ed3e65af3acd3539a4931c
ShuhangZhao <zhaoshuhang@live.cn>
2016-08-07 22:33:36 +0800

    Add Dao and entity

---
+++ TrainService/DAL/DAO/AccountDao.cs
@@ -0,0 +1,21 @@
+﻿using Arch.Data;
+using DAL.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arch.CFX;
+using Arch.CFramework;
+using Arch.Data.DbEngine;
+using System.Data;
+
+namespace DAL.DAO
+{
+    public class AccountDao
+    {
+        readonly BaseDao _baseDao = BaseDaoFactory.CreateBaseDao("userdb");
+
+        
+    }
+}
---
+++ TrainService/DAL/DAO/GoodsDao.cs
@@ -0,0 +1,43 @@
+﻿using Arch.Data;
+using DAL.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arch.CFX;
+using Arch.CFramework;
+using Arch.Data.DbEngine;
+using System.Data;
+
+namespace DAL.DAO
+{
+    public class GoodsDao
+    {
+        readonly BaseDao _baseDao = BaseDaoFactory.CreateBaseDao("userdb");
+
+        public IList<GoodsEntity> GetGoodsListByGoodsType(int goodsType)
+        {
+            var sql = "SELECT * FROM goods WHERE goods_type=@GoodsType";
+            var parameters = new StatementParameterCollection();
+            parameters.Add(new StatementParameter { Name = "@GoodsType", Direction = ParameterDirection.Input, DbType = DbType.Int32, Value = goodsType });
+            var list = _baseDao.SelectList<GoodsEntity>(sql, parameters);
+
+            return list;
+        }
+
+        public bool Add(GoodsEntity data)
+        {
+            var d = _baseDao.Insert(data);
+
+            return true;
+        }
+
+        public bool Modify(GoodsEntity data)
+        {
+            var d = _baseDao.Update(data);
+
+            return d == 1;
+        }
+    }
+}
