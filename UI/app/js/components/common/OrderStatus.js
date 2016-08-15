9f97f0c6fefe81fd01da1edeae839a0c82830b10
Huang Ling <lingling374@qq.com>
2016-08-15 22:17:25 +0800

    if cancel the order, status will only be 'Canceled'

--- UI/app/js/components/common/OrderStatus.js
+++ UI/app/js/components/common/OrderStatus.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Section, Line, Label} from '../common/Widgets';
 
-const OrderStatus = ({status, showAll}) =>{
+const OrderStatus = ({status}) =>{
      // 0：未付款，1：已付款，2：商家已接单，3：商家已配货 
     // 4:快递员已取货 5:已经送到指定位置 6：订单结束 7：订单取消 8：异常状态
     const getClass = (tabStatus) => {
@@ -16,17 +16,21 @@ const OrderStatus = ({status, showAll}) =>{
 
     return (
         <Section title='订单状态'>
-            <ul className='status direction-col'>
+           {status == 7 ?  (
+               <ul className='status direction-col'>
+                        <li className={getClass(7)}>订单取消</li>
+                </ul>)
+            : (<ul className='status direction-col'>            
                 <li className={getClass(0)}>待付款</li>
                 <li className={getClass(1)}>已付款</li>
                 <li className={getClass(2)}>商家已接单</li>
                 <li className={getClass(3)}>商家已配货</li>
                 <li className={getClass(4)}>快递员已取货</li>
                 <li className={getClass(5)}>已经送到指定位置</li>
-                <li className={getClass(7)}>订单取消</li>
                 <li className={getClass(8)}>异常状态</li>                
                 <li className={getClass(6)}>订单结束</li>
-            </ul>
+              </ul>)
+            }
         </Section>
     );
 };
