ebf670ba0769ddbd3bba842f72ee2b087a0a98e7
Huang Ling <lingling374@qq.com>
2016-08-24 23:00:51 +0800

    use svg to draw add button use same color

--- UI/app/js/components/common/NumberInput.js
+++ UI/app/js/components/common/NumberInput.js
@@ -2,11 +2,25 @@ import React, {Component} from 'react';
 
 const NumberInput = ({updateCount, count}) => (
     <div className='number-input'>
-        <button onClick={() => updateCount(Math.max(count - 1, 0))} 
-                style={{visibility: count == 0 ? 'hidden' : 'visible'}}>-</button>
+        <button onClick={() => updateCount(Math.max(count - 1, 0))}
+            style={{visibility: count == 0 ? 'hidden' : 'visible'}}>
+            <svg width="30px" height="30px" >
+                <circle cx="15px" cy="15px" r="15px" className='circle'/>
+                <line x1="5" y1="15" x2="25" y2="15" className='line'></line>
+            </svg>
+        </button>       
         <label style={{visibility: count == 0 ? 'hidden' : 'visible'}}>{count}</label>              
-        <button onClick={() => updateCount(Math.max(count + 1, 0))}>+</button>
+        <button onClick={() => updateCount(Math.max(count + 1, 0))}>
+            <svg width="30px" height="30px" >
+                <circle cx="15px" cy="15px" r="15px" className='circle'/>
+                <line x1="15" y1="5" x2="15" y2="25" className='line'></line>
+                <line x1="5" y1="15" x2="25" y2="15" className='line'></line>
+            </svg>
+        </button>
     </div>
 );
+
+// <button onClick={() => updateCount(Math.max(count - 1, 0))} 
+//                 style={{visibility: count == 0 ? 'hidden' : 'visible'}}>-</button>
 
 export default NumberInput;
